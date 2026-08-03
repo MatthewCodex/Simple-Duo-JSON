b9a81e2fd8a77e5c78da17960aa218ab3c7c8f65
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2026-08-03 14:17:58 -0400

    c

--- scripts/enemybasegen.js
+++ scripts/enemybasegen.js
@@ -11,7 +11,16 @@ Events.on(BlockBuildEndEvent, event => {
     if (event.team != null && !event.breaking) {
         
         Log.info("A " + event.tile.block() + " was placed at X: " + event.tile.x + ", Y: " + event.tile.y + "by: " + event.team);
+        
+        // Change 'router' to the specific block name you want to target
+        if (event.tile.block() === stop1times) {
+            // Your custom response code goes here
+            Vars.control.sound.stop();
+            Log.info("Music reset");
+            Vars.ui.hudfrag.showToast("Music reset");
+            
 
+        }
     }
     if (event.unit && event.unit.isPlayer()) {
         let player = event.unit.getPlayer();
