f1264ac53d8a2646b46886a92e5cc146442a60b3
MatthewCodex <147332562+MatthewCodex@users.noreply.github.com>
2026-09-12 10:49:29 -0400

    c

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,22 @@ wallBullet.frontColor = Color.valueOf('#ffffff')
 wallBullet.backColor = Color.valueOf('#00875a')
 
 const projectileWall = extendContent(Wall, "projectile-wall", {
+
+    size: 1,                    // 1x1 tile size (use 2 for a large wall)
+    health: 800,                // Amount of hitpoints the wall has
+    chanceDeflect: 10,          // 10% chance to deflect incoming bullets (like a Phase Wall)
+    flashHit: true,             // Flash white when hit
+    
+    // Requirements to build the wall
+    requirements: ItemStack.with(
+        Items.copper, 6,
+        Items.lead, 4
+    ),
+    
+    // Where it appears in the build menu
+    category: Category.defense,
+    buildVisibility: BuildVisibility.shown
+
   load(){
     this.super$load();
     this.region = Core.atlas.find("projectile-wall")
@@ -22,3 +38,4 @@ const projectileWall = extendContent(Wall, "projectile-wall", {
   }
 });
 projectileWall.shootWallBullet = projectileWall.timers++;
+//projectileWall.health = 
